8ca01f418b9b58be56a12db2a066dac99c88e436
TanSamSet <2051063845@e.tlu.edu.vn>
2023-02-22 11:44:47 +0700

    sql ex

---
+++ database/BTTH01_CSE485_ex.sql
@@ -0,0 +1,23 @@
+-- c
+SELECT * FROM theloai WHERE ma_tloai NOT IN(SELECT ma_tloai FROM baiviet WHERE ma_tloai IS NOT NULL);
+
+-- d
+SELECT baiviet.ma_bviet, baiviet.tieude, baiviet.ten_bhat, tacgia.ten_tgia, theloai.ten_tloai, baiviet.ngayviet 
+FROM baiviet 
+JOIN tacgia ON baiviet.ma_tgia = tacgia.ma_tgia JOIN theloai ON baiviet.ma_tloai = theloai.ma_tloai;
+
+-- e
+SELECT theloai.ten_tloai, COUNT(*) AS so_bviet
+FROM baiviet
+JOIN theloai ON baiviet.ma_tloai = theloai.ma_tloai
+GROUP BY baiviet.ma_tloai
+ORDER BY so_bviet DESC
+LIMIT 1;
+
+-- f
+SELECT tacgia.ten_tgia, COUNT(*) AS so_bviet
+FROM baiviet
+JOIN tacgia ON tacgia.ma_tgia = baiviet.ma_tgia
+GROUP BY baiviet.ma_tgia
+ORDER BY so_bviet DESC
+LIMIT 2;
