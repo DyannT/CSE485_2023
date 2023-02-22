b2db7218554411de9677e29a36555910c5d2d3ca
TanSamSet <2051063845@e.tlu.edu.vn>
2023-02-22 20:18:25 +0700

    up sql ex

--- database/BTTH01_CSE485_ex.sql
+++ database/BTTH01_CSE485_ex.sql
@@ -34,3 +34,64 @@ ORDER BY so_bviet DESC
 LIMIT 2;
 --h
 SELECT * FROM `baiviet` WHERE ten_bhat LIKE '%yêu%' OR ten_bhat LIKE '%thương%' OR ten_bhat LIKE '%anh%' OR ten_bhat LIKE '%em%' LIMIT 1;
+
+-- j
+DELIMITER //
+CREATE PROCEDURE sp_DSBaiViet(IN ten_tloai VARCHAR(50))
+BEGIN
+    IF NOT EXISTS(SELECT ma_tloai FROM theloai tl WHERE tl.ten_tloai = ten_tloai) THEN
+        SELECT 'Thể loại không tồn tại';
+    END IF;
+        SELECT ma_bviet, tieude, ten_bhat, ten_tgia, ten_tloai, ngayviet 
+        FROM baiviet 
+        INNER JOIN tacgia ON baiviet.ma_tgia = tacgia.ma_tgia 
+        INNER JOIN theloai ON baiviet.ma_tloai = theloai.ma_tloai 
+        WHERE theloai.ten_tloai = ten_tloai;
+END//
+DELIMITER ;
+
+-- k    
+-- Thêm mới cột SLBaiViet vào trong bảng theloai
+ALTER TABLE theloai ADD SLBaiViet INT DEFAULT 0;
+-- Tạo TRIGGER
+DROP TRIGGER IF EXISTS tg_CapNhatTheLoai_Insert;
+DELIMITER $$
+CREATE TRIGGER tg_CapNhatTheLoai_Insert
+AFTER INSERT ON baiviet
+FOR EACH ROW
+BEGIN
+  UPDATE theloai
+  SET SLBaiViet = SLBaiViet + 1
+  WHERE ma_tloai = NEW.ma_tloai;
+END$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS tg_CapNhatTheLoai_Update;
+DELIMITER $$
+CREATE TRIGGER tg_CapNhatTheLoai_Update
+AFTER UPDATE ON baiviet
+FOR EACH ROW
+BEGIN
+  IF NEW.ma_tloai <> OLD.ma_tloai THEN
+    UPDATE theloai
+    SET SLBaiViet = SLBaiViet + 1
+    WHERE ma_tloai = NEW.ma_tloai;
+
+    UPDATE theloai
+    SET SLBaiViet = SLBaiViet - 1
+    WHERE ma_tloai = OLD.ma_tloai;
+  END IF;
+END$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS tg_CapNhatTheLoai_Delete;
+DELIMITER $$
+CREATE TRIGGER tg_CapNhatTheLoai_Delete
+AFTER DELETE ON baiviet
+FOR EACH ROW
+BEGIN
+  UPDATE theloai
+  SET SLBaiViet = SLBaiViet - 1
+  WHERE ma_tloai = OLD.ma_tloai;
+END$$
+DELIMITER ;
