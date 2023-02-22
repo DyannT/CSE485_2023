aec1cbbf17cb9a528ee6a0a9d6e1f85c86cb9da4
NguyenDinhThang1204 <2051063572@e.tlu.edu.vn>
2023-02-22 23:27:44 +0700

    CSS index

--- btth01/index.php
+++ btth01/index.php
@@ -65,13 +65,14 @@
             </div>
     </header>
     <main class="container-fluid mt-3">
-        <h3 class="text-center text-uppercase mb-3 text-primary">TOP bài hát yêu thích</h3>
+        <h3 class="text-center text-uppercase mb-3">TOP bài hát yêu thích</h3>
+        <div class="container">
         <div class="row">
             <div class="col-sm-3">
                 <div class="card mb-2" style="width: 100%;">
                     <img src="images/songs/cayvagio.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title text-center">
+                        <h5 class="card-title">
                             <a href="" class="text-decoration-none">Cây, lá và gió</a>
                         </h5>
                     </div>
@@ -82,7 +83,7 @@
                 <div class="card mb-2" style="width: 100%;">
                     <img src="images/songs/csmt.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title text-center">
+                        <h5 class="card-title">
                             <a href="" class="text-decoration-none">Cuộc sống mến thương</a>
                         </h5>
                     </div>
@@ -93,7 +94,7 @@
                 <div class="card mb-2" style="width: 100%;">
                     <img src="images/songs//longme.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title text-center">
+                        <h5 class="card-title">
                             <a href="" class="text-decoration-none">Lòng mẹ</a>
                         </h5>
                     </div>
@@ -104,22 +105,22 @@
                 <div class="card mb-2" style="width: 100%;">
                     <img src="images/songs/phoipha.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title text-center">
+                        <h5 class="card-title">
                             <a href="" class="text-decoration-none">Phôi pha</a>
                         </h5>
                     </div>
                 </div>
             </div>
-
             <div class="col-sm-3">
                 <div class="card mb-2" style="width: 100%;">
                     <img src="images/songs/noitinhyeubatdau.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title text-center my-title">
+                        <h5 class="card-title my-title">
                             <a href="" class="text-decoration-none">Nơi tình yêu bắt đầu</a>
                         </h5>
                     </div>
                 </div>
+            </div>
             </div>
         </div>
     </main>
